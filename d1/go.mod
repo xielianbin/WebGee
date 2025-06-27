c4465d7e25b44ace3a0876803147d9c6e6b110f1
xielianbin <2826389624@qq.com>
2025-06-27 17:38:02 +0800

    6-27

---
+++ d1/go.mod
@@ -0,0 +1,6 @@
+module webgee
+
+go 1.24.4
+require webgee v1.0.0
+
+replace webgee => ./internal
---
+++ d1/internal/webgee.go
@@ -0,0 +1,48 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc定义了gee使用的请求处理程序
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// 引擎实现了ServeHTTP的接口
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New是gee引擎的构造函数
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET定义了添加GET请求的方法
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST定义了添加POST请求的方法
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run定义了启动http服务器的方法
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
